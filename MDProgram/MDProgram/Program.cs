f6da05fd1c17eb600b95f4593053aec0b250f88e
ManishaDanda <manishadanda2622@gmail.com>
2024-07-11 11:05:26 +1200

    Initial  Login code

---
+++ MDProgram/MDProgram/Program.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        IWebDriver driver = new ChromeDriver();
+        driver.Navigate().GoToUrl("http:/horse.industryconnect.io/");
+        driver.Manage().Window.Maximize();
+        IWebElement usernameTextbox = driver.FindElement(By.Id("UserName"));
+        usernameTextbox.SendKeys("hari");
+        IWebElement passwordTextbox = driver.FindElement(By.Id("Password"));
+        passwordTextbox.SendKeys("123123");
+        IWebElement loginbutton = driver.FindElement(By.XPath("//*[@id=\"loginForm\"]/form/div[3]/input[1]"));
+        loginbutton.Click();
+        IWebElement Hellohari = driver.FindElement(By.XPath("//*[@id=\"logoutForm\"]/ul/li/a"));
+        if (Hellohari.Text == "Hellohari") 
+        {
+
+            Console.WriteLine("user logged in successfully.test passed!");
+        }
+        else 
+        {
+            Console.WriteLine("User has not logged in.Test failed!");
+
+        }
+
+
+
+   }
+} 
